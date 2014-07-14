ea69bdb564f24847faf635117192b16f1cb1c347
Jack M <jack.m@iigins.com>
2014-07-14 14:17:35 -0700

    Updated validation to work with dates, and to set up the dates coming from the form.

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,17 +1,26 @@
+## Re-open the date class to help with the date_select form element.
+class Date
+	def self.new_from_date_select(hash, key)
+		Date.new(hash["#{key}(1i)"].to_i,
+			hash["#{key}(2i)"].to_i,
+			hash["#{key}(3i)"].to_i)
+	end
+end
+
 class Quote
 	include ActiveModel::Model
 	
 	def initialize(quote_data=nil)
 		if quote_data
-			@enter_date = quote_data.enter_date
-			@leave_date = quote.leave_date
-			@username = quote.username
-			@api_key = quote.api_key
-			if quote.policy
-				@policy = Policy.new(quote.policy)
+			@enter_date = Date.new_from_date_select(quote_data, 'enter_date')
+			@leave_date = Date.new_from_date_select(quote_data, 'leave_date')
+			@username = quote_data['username']
+			@api_key = quote_data['api_key']
+			if quote_data['policy']
+				@policy = Policy.new(quote_data.policy)
 			end
-			@power_unit = PowerUnit.new(quote)
-			@limits = Limits.new(quote)
+			# @power_unit = PowerUnit.new(quote_data)
+			# @limits = Limits.new(quote_data)
 		else
 			@enter_date = Date.today
 			@leave_date = Date.tomorrow
@@ -25,17 +34,26 @@ class Quote
 		:fixed_deductibles, :body_style, :beyond_freezone,
 		:under21, :uscoll_sc, :days_veh_in_mexico,
 		:visit_reason, :other_model
-	validates_presence_of :enter_date, :leave_date, 
-		:username, :api_key
-	validates_date :enter_date, :leave_date
-	validate :validate_enter_date
+	validates :leave_date, :date => {
+		:after_or_equal_to => :enter_date,
+		:before => Proc.new { Date.today + 365 }, ## 1 year days is too far.
+	}
+	validates :enter_date, :date => {
+		:after_or_equal_to => Proc.new { Date.today },
+		:before => Proc.new { Date.today + 90 }, ## 90 days is too far.
+	}
+	validates :year, numericality: { only_integer: true },
+		:inclusion => {:in => Proc.new { self.valid_years() } }
 
-	def valid_years
+	def self.valid_years
 		years = ((Date.today.year - 35)..Date.today.year).to_a
 		if Date.today.month >= 4
 			years << Date.today.year + 1
 		end
 		return years
+	end
+	def valid_years
+		return self.class.valid_years()
 	end
 	def valid_values
 		values = [0]
@@ -64,16 +82,8 @@ class Quote
 			4, # More than 180
 		]
 	end
+
 	private
-
-	def validate_enter_date
-		errors.add("Enter date", "is invalid") unless 
-			valid_enter_date?
-	end
-
-	def valid_enter_date?
-		false # TODO:  Make this work.
-	end	
 
 	class Policy
 		include ActiveModel::Model
