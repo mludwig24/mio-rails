573ad5eb01cbece5b7695c8ca51a8f6b07008a4d
Jack M <jack.m@iigins.com>
2014-09-11 13:46:45 -0700

    Only cache the i18n when not in development.

--- config/initializers/i18n/cache.rb
+++ config/initializers/i18n/cache.rb
@@ -1,2 +1,4 @@
-I18n::Backend::Simple.send(:include, I18n::Backend::Cache)
-I18n.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
+unless Rails.env.development?
+	I18n::Backend::Simple.send(:include, I18n::Backend::Cache)
+	I18n.cache_store = ActiveSupport::Cache.lookup_store(:memory_store)
+end
