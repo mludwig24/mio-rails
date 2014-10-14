4e4eb9fa6edd8f5b98fc05bc96ba47ba9e2668ed
Jack M <jack.m@iigins.com>
2014-10-14 09:49:30 -0700

    Removed useless back and forth.

--- db/migrate/20140919154203_add_extended_travel_to_quote.rb
+++
@@ -1,5 +0,0 @@
-class AddExtendedTravelToQuote < ActiveRecord::Migration
-  def change
-    change_column :quotes, :extended_travel, :bool, :default => true
-  end
-end
--- db/migrate/20140919154520_change_extended_travel_on_quote.rb
+++
@@ -1,5 +0,0 @@
-class ChangeExtendedTravelOnQuote < ActiveRecord::Migration
-  def change
-  	change_column :quotes, :extended_travel, :integer, :default => 1
-  end
-end
